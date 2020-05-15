ecc080aec65d3fae4f24a0f40702aada896df7b4
Rodrigo Gonzalez <rodrigo.gonzalez.zura@gmail.com>
2020-05-15 13:33:52 -0400

    feature: Se agrega nueva versión de la página con mas y mejores datos.

---
+++ new_site.js
@@ -0,0 +1,25 @@
+const cheerio = require('cheerio');
+const axios = require('axios');
+const fs = require('fs');
+
+const callMain = async totalPages => {
+    const totalSongs = [];
+    const promises = [];
+    for (let i = 0; i <= totalPages; i++) {
+        promises[i] = axios.get(`https://pumpout2.anyhowstep.com:17593/api/search/result?atVersion=154&languageCode=en&display=SONG&page=${i}&rowsPerPage=100&searchId=d43eeb9c5b0a567685c82d954502d5b3`)
+    }
+    const responses = await Promise.all(promises);
+    responses.forEach(response => {
+       totalSongs.push(response.data.rows);
+    });
+    return totalSongs;
+};
+
+callMain(8).then(response => {
+    console.log(response);
+    fs.writeFile('./songs.json', JSON.stringify(response), function (error) {
+        if (error) return console.log('Hubo un error, inténtalo nuevamente');
+        console.log('Terminado correctamente');
+    });
+});
+
